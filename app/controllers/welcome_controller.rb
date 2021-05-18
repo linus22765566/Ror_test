4c225eed929f07d475f3504cbed3128a6130742f
linus22765566 <linus2276655@gmail.com>
2021-05-18 18:12:59 +0800

    first

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+    #action: GET /welcome/say_hello
+    def say
+      #say.html.erb  
+    end
+    def index
+        
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :people
+  get "welcome/say_hello" => "welcome#say"
+  get "welcome" =>"welcome#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+end
